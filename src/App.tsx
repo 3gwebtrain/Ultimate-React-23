7d080bd82da1d754fc7c2dd5d4b4de9edbd0cdb9
3gwebtrain <3gwebtrain@gmail.com>
2023-10-21 14:30:10 +0530

    dynamic base url added

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 
 export default function App() {
+  console.log('base', import.meta.env.BASE_URL);
   return (
-    <BrowserRouter basename='Ultimate-React-23/'>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='product' element={<Product />} />
